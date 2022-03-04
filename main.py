e917cc4557e1078b5b6f1c1112c3cf5a087fc963
Raghav Aggarwal <aggarwalchirag2000@gmail.com>
2022-03-04 18:25:06 +0530

    'adding'

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 from flask import Flask
 from wsgiref import simple_server
+from werkzeug.utils import secure_filename
 import atexit
 import uuid
+import pandas as pd
 import os
-from flask import Flask, session, request, Response, jsonify,render_template
-
+from flask import Flask, session, request, Response, jsonify,render_template,redirect,url_for,flash,Markup
+from uploadsValidation import Validation
 
 app = Flask(__name__)
+# app.secret_key = "super secret key"
 
 @app.route('/',methods = ['GET'])
 def home_page():
     return render_template('home.html')
+
+@app.route('/add_data',methods = ['POST','GET'])
+def add():
+    if(request.method == 'GET'):
+        return render_template('add_csv.html')
+    elif(request.method == 'POST'):
+        try:
+
+            validation = Validation(request.files['csvfile'],'uploads','file_validation.json')
+            validation.save()
+            numberofcols,col_name = validation.checkFile()
+
+
+            # return redirect(request.url)
+
+            return render_template('result_of_add.html',data = "Data Has been added and file is validated")
+
+        except (UnicodeDecodeError,TypeError):
+            # return Response("Error : %s"% UnicodeDecodeError)
+            return render_template('result_of_add.html',data = "Please enter a .csv extension")
+
+        except ValueError:
+            return render_template('result_of_add.html', data = 'Column are not matching!')
+        except Exception as e:
+            print("abc",repr(e))
+            # return Response("Error : %s"% Exception)
+            return e
 
 port =int(os.getenv("PORT",5001))
 
@@ -21,4 +51,3 @@ if __name__ == '__main__':
     httpd.serve_forever()
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ uploadsValidation.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import os
+import numpy as np
+import json
+
+class Validation:
+    def __init__(self,file,location,jsonn):
+        self.file = file
+        self.name = location
+        self.location = "./"+location+"/" + str(file.filename)
+        self.jsonn = jsonn
+
+
+
+    def save(self):
+        try:
+            self.file.save(os.path.join(self.name,self.file.filename))
+        except Exception:
+            raise Exception
+
+
+    def checkFile(self):
+        try:
+            data = pd.read_csv(self.location)
+            with open(self.jsonn,'r') as f:
+                dic = json.load(f)
+                f.close()
+            numberofcols = dic['NumberofColumns']
+            if(data.shape[1] != numberofcols):
+                os.remove(self.location)
+                raise ValueError("abcd")
+            col_name = dic['col_name']
+            return numberofcols,col_name
+
+        except UnicodeDecodeError:
+            os.remove(self.location)
+            raise UnicodeDecodeError
+
+        except ValueError:
+            raise ValueError
+        except Exception:
+            print(Exception)
+            raise Exception
+
+
